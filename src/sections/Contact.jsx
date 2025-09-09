36a1e896d922770bcddec9864f6ce84bc02f989e
Ali Arthur <aliarthur35@gmail.com>
2025-09-09 15:07:46 -0400

    Update computer desk color to gray for the contact me section

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -102,7 +102,7 @@ const Contact = () => {
             </div>
           </div>
           <div className="xl:col-span-6 min-h-96">
-            <div className="bg-[#cd7c2e] w-full h-full hover:cursor-grab rounded-3xl overflow-hidden">
+            <div className="bg-[#2d2d38] w-full h-full hover:cursor-grab rounded-3xl overflow-hidden">
               <ContactExperience />
             </div>
           </div>
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -4,11 +4,7 @@ import { Canvas } from '@react-three/fiber';
 import { useMediaQuery } from 'react-responsive';
 import { PerspectiveCamera } from '@react-three/drei';
 
-import PythonLogo from '../components/Cube.jsx';
-import Rings from '../components/Rings.jsx';
-import ReactLogo from '../components/ReactLogo.jsx';
 import Button from '../components/Button.jsx';
-import Target from '../components/Target.jsx';
 import CanvasLoader from '../components/Loading.jsx';
 import HeroCamera from '../components/HeroCamera.jsx';
 import { calculateSizes } from '../constants/index.js';
