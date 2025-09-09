d41461fa184be238b0df147428fda16e79fa015d
Ali Arthur <aliarthur35@gmail.com>
2025-09-09 17:05:25 -0400

    update to hero section and projects section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 
 // Social media data for easy mapping
 const socialLinks = [
@@ -9,12 +8,12 @@ const socialLinks = [
 const Hero = () => {
   return (
     <section className="min-h-screen w-full flex items-center relative c-space" id="home">
-      <div className="grid lg:grid-cols-2 gap-y-20 gap-x-16 items-center w-full max-w-7xl mx-auto border-white">
-        
+      <div className="grid lg:grid-cols-2 gap-y-20 gap-x-16 items-center w-full max-w-7xl mx-auto">
+
         {/* Left Column: Text Content */}
         <div className="flex flex-col text-center lg:text-left">
           <p className="hero_tag text-gray_gradient">
-            Ali Arthur
+            Hi, I'm Ali Arthur <span className="waving-hand font-sans text-gray-200">👋</span>
           </p>
           <p className="grid-subtext mt-4 max-w-lg mx-auto lg:mx-0">
             <b>Welcome to my portfolio!</b> I am a full-stack developer with experience in building dynamic and user-focused web applications. My expertise includes front-end and back-end development, turning ideas into engaging digital experiences.
@@ -26,27 +25,27 @@ const Hero = () => {
           <div className="relative w-64 h-64 lg:w-80 lg:h-96">
             {/* The "Frame" element */}
             <div className="absolute w-full h-full bg-transparent border-4 border-neutral-700 -bottom-3 -right-3 lg:-bottom-4 lg:-right-4"></div>
-            
-            <img 
-              src="/assets/Home.jpg" 
-              alt="photo of Ali Arthur" 
+
+            <img
+              src="/assets/Home.jpg"
+              alt="photo of Ali Arthur"
               className="relative w-full h-full object-cover"
             />
-            
+
             {/* Decorative Arrow pointing to text */}
-            <img 
-              src="/assets/curved-arrow.svg" 
-              alt="Curved arrow decoration" 
+            <img
+              src="/assets/curved-arrow.svg"
+              alt="Curved arrow decoration"
               className="absolute w-16 top-1/2 -translate-y-1/2 -left-12 lg:w-24 lg:-left-20 transform -scale-x-100 rotate-[25deg]"
             />
 
             {/* Decorative Lines */}
-            <img 
-              src="/assets/random-lines.svg" 
-              alt="Random lines decoration" 
+            <img
+              src="/assets/random-lines.svg"
+              alt="Random lines decoration"
               className="absolute w-16 -bottom-10 -right-10 lg:w-20 lg:-bottom-12 lg:-right-12"
             />
-            
+
             {/* Social Links positioned to the right of the image (Large Screens Only) */}
             <div className="absolute top-1/2 right-0 transform translate-x-[150%] -translate-y-1/2 hidden lg:flex flex-col gap-4">
               {socialLinks.map((link) => (
@@ -70,7 +69,7 @@ const Hero = () => {
             </a>
           ))}
         </div>
-        
+
       </div>
     </section>
   );
